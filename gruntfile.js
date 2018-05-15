a43c9e17cf987636ae6af076ffa713f044297729
ssarhan1018 <ssarhan1018@gmail.com>
2018-05-14 23:12:53 -0700

    New grunt files

--- gruntfile.js
+++ gruntfile.js
@@ -17,30 +17,30 @@ module.exports = function(grunt) {
         *Montor files and execute taask:
         *https://www.npmjs.com/package/bearcat-grunt-contrib-watch
         */
-        watch: {
-            sass: {
-             
-                files: [
-                    'assets/scss/*.scss'
-                    
-                ],
-                tasks: [
-                    'sass'
-                ]
-            },
-            scripts: {
-             
-                files: [
-                    'assets/js/*.js'
-                    
-                ],
-                tasks: [
-                    'uglify'
-                ]
-            }
-                
-                
-        },
+//        watch: {
+//            sass: {
+//             
+//                files: [
+//                    'assets/scss/*.scss'
+//                    
+//                ],
+//                tasks: [
+//                    'sass'
+//                ]
+//            },
+//            scripts: {
+//             
+//                files: [
+//                    'assets/js/*.js'
+//                    
+//                ],
+//                tasks: [
+//                    'uglify'
+//                ]
+//            }
+//                
+//                
+//        },
         
         /**
         * Grunt Contrib uglify
@@ -48,31 +48,48 @@ module.exports = function(grunt) {
         *https://www.npmjs.com/package/grunt-contrib-uglify
         */
         
-        uglify: {
-            my_target:{
-                files: {
-                    'js/scripts.js': 'assets/js/scripts.js','node_modules/jquery/jquery.js'
-                    
-                        }
-                    
-                     }
-               },
-        
+//        uglify: {
+//            my_target:{
+//                files: {
+//                    'js/scripts.js': 'assets/js/scripts.js','node_modules/jquery/jquery.js'
+//                    
+//                        }
+//                    
+//                     }
+//               },
+//        
         /**
         * Grunt Contrib jshint
         *Montor files and execute taask:
         *https://www.npmjs.com/package/grunt-contrib-jshint
         */
         
-        jshint: {
-            files : ["*.js", "assets/js/*.js"],
-            options: {
-                esnext: true,
-                globals:{
-                    jQuery: true
-                }
-            }
-        } 
+//        jshint: {
+//            files : ["*.js", "assets/js/*.js"],
+//            options: {
+//                esnext: true,
+//                globals:{
+//                    jQuery: true
+//                }
+//            }
+//        },
+        
+        
+        /**
+        * Grunt Contrib Jasmine
+        *Montor files and execute taask:
+        *https://www.npmjs.com/package/grunt-contrib-jasmine
+        */
+        
+         jasmine: {
+    pivotal: {
+      src: 'src/*.js',
+      options: {
+        specs: 'spec/Spec.js',
+        helpers: 'spec/Helper.js'
+      }
+    }
+  }
     
 	});
     
@@ -85,6 +102,6 @@ module.exports = function(grunt) {
 	require('load-grunt-tasks')(grunt);
 
 	//Custom tasks
-	grunt.registerTask('default', ['jshint']);
+	grunt.registerTask('default', ['jasmine']);
 
 };
